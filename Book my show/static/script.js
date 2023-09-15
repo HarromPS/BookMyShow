5ffeac7e9e91ac4c791a2ceee7ae0da5462296ba
Hariom P. Shivhare <100398113+HarromPS@users.noreply.github.com>
2023-09-15 11:27:45 +0530

    Add files via upload

---
+++ Book my show/static/script.js
@@ -0,0 +1,39 @@
+console.log("Welcome to BookMyShow");
+
+
+let slideIndex = 0;
+
+function showSlides() {
+    let i;
+
+    // get the array of divs' with classname image-sliderfade
+    let slides = document.getElementsByClassName("image-sliderfade");
+
+    // get the array of divs' with classname dot
+    let dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        // initially set the display to
+        // none for every image.
+        slides[i].style.display = "none";
+    }
+
+    // increase by 1, Global variable
+    slideIndex++;
+
+    // check for boundary
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    // Change image every 2 seconds
+    setTimeout(showSlides, 2000);
+}
+showSlides(); // call showslide method
